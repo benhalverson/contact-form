96dd09ea71cb7929b42d401e80e78abd80b70c62
Ben Halverson <benhalverson33@gmail.com>
2023-09-20 11:10:55 -0700

    formatting

--- index.js
+++ index.js
@@ -1,38 +1,42 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const nodemailer = require('nodemailer')
+const express = require("express");
+const dotenv = require("dotenv");
+const nodemailer = require("nodemailer");
 
-dotenv.config()
-const PORT = process.env.PORT || 3000
-const app = express()
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.post('/send-email', (req, res) => {
-  const { name, email, message } = req.body
+app.post("/send-email", (req, res) => {
+  const { name, email, message } = req.body;
 
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    service: "gmail",
     auth: {
       user: process.env.USER,
-      pass: process.env.PASS
-  }})
+      pass: process.env.PASS,
+    },
+  });
 
-  transporter.sendMail({
-    from: email,
-    to: 'myemail@email.com',
-    subject: `hello ${name}`,
-    text: message
-  }).then(() => {
-    return res.send({
-      message: 'Email sent successfully',
+  transporter
+    .sendMail({
+      from: email,
+      to: "myemail@email.com",
+      subject: `hello ${name}`,
+      text: message,
     })
-  }).catch(error => {
-    console.error('error', error)
-    return res.send({
-      message: 'Error sending email',
+    .then(() => {
+      return res.send({
+        message: "Email sent successfully",
+      });
     })
-  })
+    .catch((error) => {
+      console.error("error", error);
+      return res.send({
+        message: "Error sending email",
+      });
+    });
 });
 
-app.listen(PORT, () => console.log('server started on port 3000'))
+app.listen(PORT, () => console.log("server started on port 3000"));
